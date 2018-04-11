bb06606f684e8e52a115434130d7ad6f05d95581
Michał Bohdanowicz <s165759@student.pg.gda.pl>
2018-04-11 09:09:07 +0200

    coś

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -6,7 +6,7 @@
 #define MATRIX_MATRIX_H
 
 #include <array>
-#include <assert.h>
+#include <cassert>
 #include <iostream>
 
 namespace Matrix {
@@ -15,6 +15,8 @@ namespace Matrix {
     class DenseMatrix{
 
         value_t values[N * M];
+
+        typedef DenseMatrix<value_t, N, M> matrix_t;
 
     public:
 
@@ -40,17 +42,36 @@ namespace Matrix {
             return values[n * cols + m];
         }
 
-        template<
-                class matrix_t,
-                class matrix_out_t = DenseMatrix<value_t, rows, matrix_t::cols>
-        >
-        matrix_out_t&& operator*(matrix_t& multi) {
-            static_assert(cols == matrix_t::rows, "Matrix dimensions mismatch");
 
-            matrix_out_t a;
+//        Working implementation
+//        template<
+//                class matrix_t,
+//                class matrix_out_t = DenseMatrix<value_t, rows, matrix_t::cols>
+//        >
+//        matrix_out_t&& operator*(matrix_t& multi) {
+//
+//            static_assert(cols == matrix_t::rows, "Matrix dimensions mismatch");
+//
+//            matrix_out_t a;
+//
+//            for (size_t i = 0; i < rows; ++i) {
+//                for (size_t j = 0; j < matrix_t::cols; ++j) {
+//                    a(i, j) = 0;
+//                    for (size_t k = 0; k < cols; ++k) {
+//                        a(i, j) += (*this) (i, k) * multi(k, j);
+//                    }
+//                }
+//            }
+//
+//            return std::move(a);
+//        }
+
+        template <typename T, size_t C>
+        DenseMatrix<T, rows, C>&& operator*(DenseMatrix<T, cols, C>& multi) {
+            DenseMatrix<T, rows, C> a;
 
             for (size_t i = 0; i < rows; ++i) {
-                for (size_t j = 0; j < matrix_t::cols; ++j) {
+                for (size_t j = 0; j < C; ++j) {
                     a(i, j) = 0;
                     for (size_t k = 0; k < cols; ++k) {
                         a(i, j) += (*this) (i, k) * multi(k, j);
@@ -61,15 +82,21 @@ namespace Matrix {
             return std::move(a);
         }
 
-//        template<
-//                class matrix_t,
-//                class matrix_out_t = DenseMatrix<value_t, rows, matrix_t::cols>
-//        >
+        matrix_t&& operator+(matrix_t& add){
+            matrix_t a;
 
-        template <class matrix_t>
+            for (size_t i = 0; i < rows; ++i) {
+                for (size_t j = 0; j < cols; ++j) {
+                    a(i, j) = add(i, j) + (*this)(i, j);
+                }
+            }
+
+            return std::move(a);
+        };
+
         friend std::ostream& operator<< (std::ostream& stream, matrix_t& m){
 
-            stream << "dimensions: (" << matrix_t::rows << " x " << matrix_t::cols << ')' << std::endl;
+            stream << "dimensions: (" << rows << " x " << matrix_t::cols << ')' << std::endl;
 
             for (size_t i = 0; i < matrix_t::rows; ++i) {
                 for (size_t j = 0; j < matrix_t::cols; ++j) {
--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -4,8 +4,6 @@
 
 int main() {
 
-    std::cout << "Hello, World!" << std::endl;
-
     Matrix::DenseMatrix<int, 3, 3> A {{2,0,0}, {0,2,0}, {0,0,2}}, B {{5, 4, 3}, {2, 3, 6}, {3, 4, 4}};
     Matrix::DenseMatrix<int, 3, 4> D {{2,3,4,5}, {6,7,8,9}, {1,2,3,4}};
 
